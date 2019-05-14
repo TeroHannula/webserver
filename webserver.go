ccba190047754177bd975c0811b65fc3f228dd8b
Tero Hannula <terohan@gmail.com>
2019-05-14 23:27:14 +0300

    First commit, the most simple working service

---
+++ webserver.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	startHTTPserver()
+}
+
+func startHTTPserver() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/list", listHandler)
+	mux.HandleFunc("/details", detailsHandler)
+	port := ":8090"
+	fmt.Println("Starting, port is", port)
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal("Web server did not start")
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Index")
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List")
+}
+
+func detailsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Details")
+}
